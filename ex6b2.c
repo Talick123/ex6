e10c34542657485c777a647deb581d99a84ce0f5
nogalevy <nogalevy2@gmail.com>
2021-12-18 18:56:00 +0200

    add b not delete the socket

--- ex6b2.c
+++ ex6b2.c
@@ -12,3 +12,164 @@ inferno-03 = 10.3.10.25
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define ARR_SIZE 1000
+#define ARGC_SIZE 2 // port
+#define NUM_OF_CLIENTS 3
+
+// --------const and enum section------------------------
+
+const int LEN_STR_MAX = 100;
+
+// --------prototype section------------------------
+
+void check_argc(int argc);
+int init_socket(char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res);
+void get_requests(int main_socket);
+void wait_for_clients(fd_set rfd, int main_socket);
+int count_appearances(int arr[], int filled, int num);
+void print_data(int new_prime, int max, int min);
+void perrorandexit(char *msg);
+int is_pal(char str[], int n);
+
+// --------main section------------------------
+
+int main(int argc, char *argv[])
+{
+    int main_socket;
+    struct addrinfo con_kind, *addr_info_res = NULL;
+
+    check_argc(argc);
+    main_socket = init_socket(argv[1], con_kind, addr_info_res);
+
+    get_requests(main_socket);
+    close(main_socket);
+    freeaddrinfo(addr_info_res);
+
+    return EXIT_SUCCESS;
+}
+
+//-------------------------------------------------
+
+void get_requests(int main_socket)
+{
+    int rc, fd, res;
+    char str[LEN_STR_MAX],
+	fd_set rfd ,c_rfd;
+
+	rc = listen(main_socket,NUM_OF_CLIENTS);
+	if(rc)
+	{
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
+	FD_ZERO(&rfd);
+	FD_SET(main_socket ,&rfd);
+
+	while(1)
+	{
+		c_rfd = rfd;
+		rc = select(getdtablesize() ,&c_rfd ,NULL ,NULL ,NULL);
+
+		if(FD_ISSET(main_socket ,&c_rfd))
+		{
+			serving_socket = accept(main_socket ,NULL ,NULL);
+			if(serving_socket >= 0)
+			{
+				FD_SET(serving_socket ,&rfd);
+			}
+		}
+
+		for(fd = main_socket + 1; fd < getdtablesize();fd++)
+		{
+			if(FD_ISSET(fd ,&c_rfd))
+			{
+				rc = read(fd ,&str ,sizeof(char)*LEN_STR_MAX);
+				if(rc > 0)
+				{
+					res = is_pal(str, strlen(str));
+					write(fd ,&res ,sizeof(int));
+				}
+				else
+				{
+					perror("read() failed");
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+	}
+}
+
+//-------------------------------------------------
+
+void check_argc(int argc)
+{
+	if(argc != ARGC_SIZE)
+		perrorandexit("Error! Incorrect number of arguments\n");
+}
+
+
+//-------------------------------------------------
+
+int init_socket(char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res)
+{
+    int main_socket, rc;
+
+    printf("Port is: %s\n", port);
+
+    memset(&con_kind, 0, sizeof(con_kind));
+    con_kind.ai_family = AF_UNSPEC;
+    con_kind.ai_socktype = SOCK_STREAM;
+    con_kind.ai_flags = AI_PASSIVE;
+
+    if((rc = getaddrinfo(NULL, port, &con_kind, &addr_info_res)!= 0))
+        perrorandexit("getaddrinfo failed\n");
+
+    main_socket = socket(addr_info_res->ai_family, addr_info_res->ai_socktype, addr_info_res->ai_protocol);
+
+    if(main_socket < 0)
+        perrorandexit("socket failed\n");
+
+    rc = bind(main_socket, addr_info_res->ai_addr, addr_info_res->ai_addrlen);
+
+    if(rc)
+        perrorandexit("bind failed\n");
+
+    return main_socket;
+}
+
+//-------------------------------------------------
+
+int is_pal(char str[], int n)
+{
+    // Initialise flag to zero.
+    int flag = 1;
+
+    // Loop till array size n/2.
+    for (int i = 0; i <= n / 2 && n != 0; i++)
+    {
+        // Check if first and last element are different
+        // Then set flag to 1.
+        if (str[i] != str[n - 1- i ]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    // If flag is set then print Not Palindrome
+    // else print Palindrome.
+    return flag;
+}
+
+//-------------------------------------------------
--- ex6b3.c
+++ ex6b3.c
@@ -14,6 +14,220 @@ in a loop:
 inferno-03 = 10.3.10.25
 */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+
+#define ARGC_SIZE 5
+
+const int LEN_STR_MAX = 100;
+
+// --------prototype section------------------------
+
+void check_argc(int argc);
+int connect_to_socket(char ip[], char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res);
+
+
+// --------main section------------------------
+
+int main(int argc, char *argv[])
+{
+    int my_socket_p, my_socket_q;
+    struct addrinfo con_kind, *addr_info_res = NULL;
+
+    check_argc(argc);
+    srand(atoi(argv[1]));
+    srand(atoi(argv[3]));
+
+    my_socket_p = connect_to_socket(argv[2], argv[3], con_kind, addr_info_res);
+    my_socket_q = connect_to_socket(argv[4], argv[5], con_kind, addr_info_res);
+
+    get_user_req(my_socket_p, my_socket_q);
+    close(my_socket);
+    freeaddrinfo(addr_info_res);
+
+    return EXIT_SUCCESS;
+}
+//-------------------------------------------------
+
+void get_user_req(my_socket_p, my_socket_q)
+{
+    int rc;
+    char c;
+
+    while(true)
+    {
+        c = getchar();
+        switch (c)
+        {
+            //read num to check is prime
+            case 'p':
+                p_request(my_socket_p);
+                break;
+            //read series of num to check is palindrome
+            case 'q':
+                q_request(my_socket_q);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+//------------------------------------------------
+
+void p_request(int my_socket_p)
+{
+    int num, res;
+    int rc;
+
+    scanf(" %d", &num);
+    getchar();
+
+    shm_ptr_p[P_NUM] = num;
+
+    write(my_socket_p ,&num ,sizeof(int));
+    rc = read(my_socket_p ,&res ,sizeof(int));
+    if(rc > 0)
+    {
+        printf("is prime ? %s\n", res ? "yes" : "no");
+    }
+    else
+    {
+        perrorandexit("read() failed");
+    }
+}
+
+//------------------------------------------------
+
+void q_request()
+{
+    int num, i;
+    char str[LEN_STR_MAX];
+    char c;
+    for(i = 0; i < LEN_STR_MAX; i++)
+    {
+        c = getchar();
+        //N: read to '0' ?
+        if(c == '0')
+        {
+            str[i] = '\0'; // null?
+            break;
+        }
+        else
+            str[i] = c;
+    }
+    getchar();
+
+    write(my_socket_q ,&str ,sizeof(char)*LEN_STR_MAX);
+    rc = read(my_socket_q ,res ,sizeof(int)*MAX_AMOUNT);
+    if(rc > 0)
+    {
+        printf("is palindrome ? %s\n", res ? "yes" : "no");
+    }
+    else
+    {
+        perror("read() failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//-------------------------------------------------
+
+void check_argc(int argc)
+{
+	if(argc != ARGC_SIZE)
+		perrorandexit("Error! Incorrect number of arguments\n");
+}
+
+//-------------------------------------------------
+
+int connect_to_socket(char ip[], char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res)
+{
+    int my_socket, rc;
+
+	printf("Port is: %s\n", port);
+	printf("IP is: %s\n", ip);
+
+    memset(&con_kind, 0, sizeof(con_kind));
+    con_kind.ai_family = AF_UNSPEC;
+    con_kind.ai_socktype = SOCK_STREAM;
+
+    if((rc = getaddrinfo(ip, port, &con_kind, &addr_info_res)!= 0))
+        perrorandexit("getaddrinfo failed\n");
+
+    my_socket = socket(addr_info_res->ai_family, addr_info_res->ai_socktype, addr_info_res->ai_protocol); //returns file descriptor or -1 if failed
+
+    if(my_socket < 0)
+        perrorandexit("socket failed\n");
+
+    rc = connect(my_socket, addr_info_res->ai_addr, addr_info_res->ai_addrlen);
+
+    if(rc) //0 if succeeded, -1 if not
+        perrorandexit("connect failed\n");
+
+    return my_socket;
+}
+
+
+
+
+//
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <netdb.h>
+//
+//
+// int main(int argc, char *argv[])
+// {
+//     int rc;
+//     int my_socket;
+//     // int i, num // ?
+//     char rbuf[BUFLEN];
+//     struct addrinfo con_kind, *addr_info_res;
+//
+//     //check argc
+//     if(argc < 2)
+//     {
+// 		fprintf(stderr, "Missing");
+// 		exit(EXIT_FAILURE);
+// 	}
+//
+//     memset(&con_kind, 0, size(con_kind));
+//     con_kind.ai_family = AF_UNSPEC;
+//     con_kind.ai_socktype = SOCK_STREAM;
+//
+//     if((rc = getaddrinfor(/*SERVER IP*/, /*SERVER PORT*/, &con_kind, &addr_info_res)!= 0))
+//     {
+// 		perrorandexit();
+// 	}
+//
+//     my_socket = socket(addr_info_res->ai_family, addr_info_res->ai_socktype, addr_info_res->ai_protocol);
+//
+//     if(my_socket < 0)
+//         perrorandexit();
+//
+//     rc = connect(my_socket, addr_info_res->ai_addr, addr_info_res->ai_addrlen);
+//
+//     if(rc)
+//         perrorandexit();
+//
+// 	while(1)
+// 	{
+// 		puts("enter a number");
+// 		scanf("%d", &num);
+// 		if(!num) break;
+//
+// 		write(my_socket, &num, rbuf, BUFLEN);
+// 		rc = read(my_socket, rbuf, BUFLEN);
+//
+// 		if(rc > 0)
+// 			printf("%s\n", rbuf);
+// 		else
+// 		{
+// 			perrorandexit() // read failed
+// 		}
+// 	}
+//
+// 	close(my_soscket);
+// 	freeaddrinfo(addr_info_res);
+//
+//     return EXIT_SUCCESS;
+// }
