ffa13c442f204a27090c7ff5532c25b0ada0b494
Tali Kalev <talkalev@gmail.com>
2021-12-18 15:37:21 +0200

    ex6a2.c and ex6a2.c compile

--- ex6a1.c
+++ ex6a1.c
@@ -6,43 +6,252 @@
 - from each client, reads number, adds to array
 - sends feedback to client that sent the number
 - when finished to fill array, sends to clients -1
-- Output: same as previous
+Output:
+    From main process (ex6a1) = minimum prime, max prime and number of
+    different numbers in the array.
+    Example: The number of different primes received is: 168
+             The max prime is: 997. The min primes is: 2
+    From sub process (ex6a2) = prime number they send the most to main process
+    Example: Process 1101373 sent the prime 233, 14 times
 
 inferno-03 = 10.3.10.25
 */
+// --------include section------------------------
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
-int main(/*arc*/)
+#define ARR_SIZE 1000
+#define ARGC_SIZE 2
+#define NUM_OF_CLIENTS 3
+
+// --------const and enum section------------------------
+
+//enum Status {START = 1, END = -1};
+const int START = 1;
+const int END = -1;
+
+// --------prototype section------------------------
+
+void check_argc(int argc);
+int init_socket(char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res);
+void fill_arr(int main_socket);
+void wait_for_clients(fd_set rfd, int main_socket);
+int count_appearances(int arr[], int filled, int num);
+void print_data(int new_prime, int max, int min);
+void perrorandexit(char *msg);
+
+// --------main section------------------------
+
+int main(int argc, char *argv[])
 {
+    int main_socket;
+    struct addrinfo con_kind, *addr_info_res = NULL;
 
-    int rc;
-    int my_socket;
-    char rbuf[BUFLEN];
-    struct addrinfo con_kind, *addr_info_res;
+    check_argc(argc);
+    main_socket = init_socket(argv[1], con_kind, addr_info_res);
+    fill_arr(main_socket);
+    close(main_socket);
+    freeaddrinfo(addr_info_res);
 
-    //check argc
+    return EXIT_SUCCESS;
+}
 
-    memset(&con_kind, 0, size(con_kind));
+//-------------------------------------------------
+
+void check_argc(int argc)
+{
+	if(argc != ARGC_SIZE)
+		perrorandexit("Error! Incorrect number of arguments\n");
+}
+
+//-------------------------------------------------
+
+int init_socket(char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res)
+{
+    int main_socket, rc;
+
+    memset(&con_kind, 0, sizeof(con_kind));
     con_kind.ai_family = AF_UNSPEC;
     con_kind.ai_socktype = SOCK_STREAM;
+    con_kind.ai_flags = AI_PASSIVE;
 
-    if((rc = getaddrinfor(/*SERVER IP*/, /*SERVER PORT*/, &con_kind, &addr_info_res)!= 0))
-        perrorandexit();
+    if((rc = getaddrinfo(NULL, port, &con_kind, &addr_info_res)!= 0))
+        perrorandexit("getaddrinfo failed\n");
 
-    my_socket = socket(addr_info_res->ai_family, addr_info_res->ai_socktype, addr_info_res->ai_protocol);
+    main_socket = socket(addr_info_res->ai_family, addr_info_res->ai_socktype, addr_info_res->ai_protocol);
 
-    if(my_socket < 0)
-        perrorandexit();
+    if(main_socket < 0)
+        perrorandexit("socket failed\n");
 
-    rc = connect(my_socket, addr_info_res->ai_addr, addr_info_res->ai_addrlen);
+    rc = bind(main_socket, addr_info_res->ai_addr, addr_info_res->ai_addrlen);
 
     if(rc)
-        perrorandexit();
+        perrorandexit("bind failed\n");
 
-    
-    return EXIT_SUCCESS;
+    return main_socket;
+}
+
+//-------------------------------------------------
+
+void fill_arr(int main_socket)
+{
+    int rc, serving_socket, filled = 0, num, new_prime = 0, min = 0, max = 0, count;
+    int arr[ARR_SIZE];
+    fd_set rfd, c_rfd;
+    int fd;
+
+    rc = listen(main_socket, NUM_OF_CLIENTS); //T: 3?
+    if(rc)
+        perrorandexit("listen failed\n");
+
+    FD_ZERO(&rfd);
+    FD_SET(main_socket, &rfd);
+
+    wait_for_clients(rfd, main_socket);
+
+    //filling array
+    while(filled < ARR_SIZE)
+    {
+        c_rfd = rfd;
+
+        rc = select(getdtablesize(), &c_rfd, NULL, NULL, NULL); //check rc?
+
+        if(FD_ISSET(main_socket, &c_rfd))
+        {
+            serving_socket = accept(main_socket, NULL, NULL);
+            if(serving_socket >= 0)
+                FD_SET(serving_socket, &rfd);
+        }
+
+        for(fd = main_socket + 1; fd < getdtablesize(); fd++)
+        {
+            if(FD_ISSET(fd, &c_rfd))
+            {
+                rc = read(fd, &num, sizeof(int));
+                if(rc > 0)
+                {
+                    if(filled >= ARR_SIZE)
+                    {
+                        write(fd, &END, sizeof(int));
+                        close(fd);
+                        FD_CLR(fd, &rfd);
+                    }
+                    else
+                    {
+                        count = count_appearances(arr, filled, num);
+                        write(fd, &count, sizeof(int));
+
+                        if(count == 0)
+                            new_prime++;
+                        if(num > max)
+                            max = num;
+                        if(num < min || min == 0)
+                            min = num;
+
+                        arr[filled] = num;
+                        filled++;
+                    }
+                }
+                else if(rc == 0)
+                {
+                    close(fd);
+                    FD_CLR(fd, &rfd);
+                }
+                else
+                    perrorandexit("read failed\n");
+            }
+        }
+    }
+
+    //closing any remaing waiting clients
+    for(fd = main_socket + 1; fd < getdtablesize(); fd++)
+    {
+        if(FD_ISSET(fd, &c_rfd))
+        {
+            rc = read(fd, &num, sizeof(int));
+            if(rc > 0)
+            {
+                write(fd, &END, sizeof(int));
+                close(fd);
+                FD_CLR(fd, &rfd);
+            }
+		}
+    }
+
+    print_data(new_prime, max, min);
+
+}
+
+//-------------------------------------------------
+
+void wait_for_clients(fd_set rfd, int main_socket)
+{
+    int rc, client_count = 0, serving_socket, fd, answer;
+    fd_set c_rfd;
+
+    //receiving ones from all clients
+    while(client_count !=  NUM_OF_CLIENTS)
+    {
+        c_rfd = rfd;
+        rc = select(getdtablesize(), &c_rfd, NULL, NULL, NULL); //check rc?
+        if(FD_ISSET(main_socket, &c_rfd))
+        {
+            serving_socket = accept(main_socket, NULL, NULL);
+            if(serving_socket >= 0)
+                FD_SET(serving_socket, &rfd);
+        }
+        for(fd = main_socket + 1; fd < getdtablesize(); fd++)
+        {
+            if(FD_ISSET(fd, &c_rfd))
+            {
+                rc = read(fd, &answer, sizeof(int));
+                if(rc > 0)
+                    client_count++;
+            }
+            if(client_count == NUM_OF_CLIENTS)
+                break;
+        }
+    }
+
+    //sending to clients to start
+    for(fd = main_socket + 1; fd < getdtablesize(); fd++)
+        write(fd, &START, sizeof(int));
+
+}
+
+//-------------------------------------------------
+
+int count_appearances(int arr[], int filled, int num)
+{
+    int counter = 0, index;
+
+    for(index = 0; index < filled; index++)
+        if(arr[index] == num)
+            counter++;
+
+    return counter;
+}
+
+//-------------------------------------------------
+
+void print_data(int new_prime, int max, int min)
+{
+	printf("The number of different primes received is: %d\n", new_prime);
+	printf("The max prime is: %d\n. The min prime is: %d\n", max, min);
+}
+
+
+//-------------------------------------------------
+
+void perrorandexit(char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
--- ex6a2.c
+++ ex6a2.c
@@ -15,8 +15,161 @@
 - to start, client sends message to server that its ready, waits for answer to start
 - when done, prints output like last time
 
+Output:
+    From main process (ex6a1) = minimum prime, max prime and number of
+    different numbers in the array.
+    Example: The number of different primes received is: 168
+             The max prime is: 997. The min primes is: 2
+    From sub process (ex6a2) = prime number they send the most to main process
+    Example: Process 1101373 sent the prime 233, 14 times
+
 */
+// --------include section------------------------
+
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARGC_SIZE 4
+
+// --------const and enum section------------------------
+
+//enum Status {START = 1, END = -1};
+const int START = 1;
+const int END = -1;
+
+// --------prototype section------------------------
+
+void check_argc(int argc);
+int connect_to_socket(char ip[], char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res);
+void generate_numbers(int my_socket);
+bool prime(int num);
+void print_data(int max_count,int max_prime ,int new_primes);
+void perrorandexit(char *msg);
+
+// --------main section------------------------
+
+int main(int argc, char *argv[])
+{
+    int my_socket;
+    struct addrinfo con_kind, *addr_info_res = NULL;
+
+    check_argc(argc);
+    srand(atoi(argv[1]));
+    my_socket = connect_to_socket(argv[2], argv[3], con_kind, addr_info_res);
+    generate_numbers(my_socket);
+    close(my_socket);
+    freeaddrinfo(addr_info_res);
+
+    return EXIT_SUCCESS;
+}
+//-------------------------------------------------
+
+void check_argc(int argc)
+{
+	if(argc != ARGC_SIZE)
+		perrorandexit("Error! Incorrect number of arguments\n");
+}
+//-------------------------------------------------
+
+int connect_to_socket(char ip[], char port[], struct addrinfo con_kind, struct addrinfo *addr_info_res)
+{
+    int my_socket, rc;
+
+    memset(&con_kind, 0, sizeof(con_kind));
+    con_kind.ai_family = AF_UNSPEC;
+    con_kind.ai_socktype = SOCK_STREAM;
+
+    if((rc = getaddrinfo(ip, port, &con_kind, &addr_info_res)!= 0))
+        perrorandexit("getaddrinfo failed\n");
+
+    my_socket = socket(addr_info_res->ai_family, addr_info_res->ai_socktype, addr_info_res->ai_protocol); //returns file descriptor or -1 if failed
+
+    if(my_socket < 0)
+        perrorandexit("socket failed\n");
+
+    rc = connect(my_socket, addr_info_res->ai_addr, addr_info_res->ai_addrlen);
+
+    if(rc) //0 if succeeded, -1 if not
+        perrorandexit("connect failed\n");
+
+    return my_socket;
+}
+
+
+//-------------------------------------------------
+
+void generate_numbers(int my_socket)
+{
+    int rc;
+    int max_count, max_prime, num, new_primes, answer;
+     max_count = new_primes = 0;
+
+    //writing to server that its ready to start
+    write(my_socket, &START , sizeof(int));
+    rc = read(my_socket, &answer, sizeof(int));
+
+    if(answer != START) //just to check
+        printf("%d", answer);
+
+    while(answer != END)
+    {
+        num = (rand() + 2);
+
+        if(prime(num))
+        {
+            write(my_socket, &num, sizeof(int));
+            rc = read(my_socket, &answer, sizeof(int));
+            if(rc > 0)
+            {
+                if(answer > max_count)
+                {
+                    max_count = answer;
+                    max_prime = num;
+                }
+                if(answer == 0)
+                    new_primes++;
+            }
+            else
+                perrorandexit("read failed\n");
+        }
+    }
+    print_data(max_count, max_prime, new_primes);
+}
+
+//-------------------------------------------------
+//check is prime
+bool prime(int num)
+{
+	int i;
+	for(i = 2; i*i <= num; i++)
+		if(num % i == 0)
+			return false;
+
+	return true;
+}
+
+//-------------------------------------------------
+
+void print_data(int max_count,int max_prime ,int new_primes)
+{
+    printf("Process %d sent %d different new primes.\n",
+        (int)getpid(), new_primes);
+
+     printf("The prime it sent most was %d, %d times \n",
+        	   max_prime, max_count);
+}
+
+//-------------------------------------------------
+
+void perrorandexit(char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
